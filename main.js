cc4cf275b807484ba2cd39f00749a9fd34dca35a
Marina Perin <maryfrancisco@live.com>
2023-11-16 16:14:17 +0100

    feat | exercises 1-2

--- main.js
+++ main.js
@@ -10,3 +10,25 @@
     meno.
 */
 
+const safeParseJSON = (string) => {
+    try{
+        const stringAnalysis = JSON.parse(string);
+        console.log('The analysis was a success');
+    }catch(error){
+        console.log('The analysis was unsuccesful');
+        return null;
+    }
+};
+
+/*
+    Exercise 2    
+
+    Crea una funzione chiamata logElements che 
+    prende un array e stampa in console ogni elemento 
+    utilizzando il metodo .forEach().
+*/
+
+const logElements = (array)=>{
+    array.forEach( el => console.log(el));
+};
+
