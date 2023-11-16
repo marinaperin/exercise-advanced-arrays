93256351d96b23e89df508f77b17aa72450bf0c3
Marina Perin <maryfrancisco@live.com>
2023-11-16 15:53:57 +0100

    feat | boilerplate and linked js

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+    Exercise 1
+    
+    Crea una funzione safeParseJSON che prende 
+    una stringa e tenta di analizzarla come JSON.
+    Se l'analisi fallisce, la funzione dovrebbe 
+    restituire null invece di generare un errore.
+    Inoltre, la funzione dovrebbe stampare in 
+    console se l'analisi è stata un successo o 
+    meno.
+*/
+
